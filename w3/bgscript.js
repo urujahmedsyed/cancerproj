ed72ef92242c1600fb302f49aeecdd29b53faf48
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2022-12-04 08:22:57 +0530

    Update bgscript.js

--- w3/bgscript.js
+++ w3/bgscript.js
@@ -3,7 +3,7 @@ const txt=document.getElementById("txt");
 const body=document.body;
 const colors=['rgb(236, 170, 236)','rgb(119, 86, 143)','rgb(139, 139, 255)','rgb(92, 137, 92)','rgb(255, 255, 161)','rgb(254, 212, 135)','rgb(253, 174, 174)'];
 btn.addEventListener('click',chbg);
-var i=0;
+var i=-1;
 function chbg(){
     i=i+1;
     if(i==7){
